56774582b9e3a041f5dadda0fe242e9aec5536b5
mickael <mickael@yaakadev.com>
2023-12-17 00:43:07 +0100

    .

--- Api/src/main.ts
+++ Api/src/main.ts
@@ -7,13 +7,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService: ConfigService = app.get(ConfigService);
-  const frontendUrls = configService.get<string>('FRONTEND_URL').split(',');
-  console.log('front url',frontendUrls);
+  // const frontendUrls = configService.get<string>('FRONTEND_URL').split(',');
+  // console.log('front url',frontendUrls);
   app.enableCors({
     // ne pas oublier d'enlever le _DEV pour la prod
     // origin: '*',
-    origin: frontendUrls,
+    origin: configService.get<string>('FRONTEND_URL'),
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // allowedHeaders: [
+    //   'Content-Type, Accept , Authorization , X-Requested-With , X-Forwarded-for , X-Amzn-Trace-Id , Access-Control-Allow-Origin'
+    // ],
     credentials: true,
   });
 
