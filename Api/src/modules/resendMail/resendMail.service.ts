69fd867e13efc7b261e95da78f10ca3d057ffc2c
mickael <mickael@yaakadev.com>
2025-04-14 19:27:35 +0200

    [feat] : put back core

--- Api/src/modules/resendMail/resendMail.service.ts
+++ Api/src/modules/resendMail/resendMail.service.ts
@@ -6,18 +6,18 @@ import { Resend} from 'resend';
 export class ResendService {
     public constructor(@InjectResend() private readonly resendClient: Resend) {}
 
-    public async sendEmail() {
-        try {
-            return await this.resendClient.emails.send({
-                from: 'mickael.raveneau@devnightspace.com',
-                to: 'mickael.raveneau@devnightspace.com',
-                subject: 'Howdy!',
-                html: '<strong>YAY!</strong>',
-            });
-        } catch (error) {
-            console.error('Error sending email:', error);
-            throw error;
-        }
-    }
+    // public async sendEmail() {
+    //     try {
+    //         return await this.resendClient.emails.send({
+    //             from: 'mickael.raveneau@devnightspace.com',
+    //             to: 'mickael.raveneau@devnightspace.com',
+    //             subject: 'Howdy!',
+    //             html: '<strong>YAY!</strong>',
+    //         });
+    //     } catch (error) {
+    //         console.error('Error sending email:', error);
+    //         throw error;
+    //     }
+    // }
     
 }
